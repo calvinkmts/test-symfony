593158d0c8166805bff18e1937c8404f153f75d8
Calvin Kamtoso <calvinkmts@gmail.com>
2023-06-06 03:16:04 +0700

    feat: trying out controller and route on symfony

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,23 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class HomeController
 {
+    public function index(): Response
+    {
+        return new Response(
+            '<html><body>Home page</body></html>'
+        );
+    }
 
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
 }
--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,7 +2,16 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class LuckyController
 {
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
 
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
 }
